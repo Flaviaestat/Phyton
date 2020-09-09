ddaebdb913944789ed82837c58ea973e2d0bf633
Flávia Costa <flaviaestat@gmail.com>
2020-09-09 18:33:26 -0300

    Add files via upload

---
+++ RunGAScripts.bat
@@ -0,0 +1,4 @@
+@echo on
+call C:\Users\FlaviaCosta\anaconda3\Scripts\activate.bat
+C:\Users\FlaviaCosta\anaconda3\python.exe "Google analytics Site Info.py"
+C:\Users\FlaviaCosta\anaconda3\python.exe "Google analytics Site Info_anual.py"
